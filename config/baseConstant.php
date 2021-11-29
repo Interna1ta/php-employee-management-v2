a2dc30cbe1fe6ce19a1dcfd9e15e8b906bedd3e9
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-11-29 14:32:34 +0100

    Bootstrap Integrated

--- config/baseConstant.php
+++ config/baseConstant.php
@@ -1,6 +1,6 @@
 <?php
 
-$baseUrl = getcwd();
-$baseUrl = 'http://localhost/php-employee-management-v2';
+$baseUrl = $_SERVER["REQUEST_URI"];
+// $baseUrl = 'http://localhost/php-employee-management-v2';
 
 define("BASE_URL", $baseUrl);
